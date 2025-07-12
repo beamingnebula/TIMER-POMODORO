1aa0be14c1156db434ed4dbc1796e9bee1ae8221
beamingnebula <mail2mehboobali@gmail.com>
2025-07-12 22:59:59 +0600

    This project is now Open Source!!!

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'timer-app-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/downloadpage.html',
+    '/timer-end.mp3',
+    '/android-chrome-192x192.png',
+    '/android-chrome-512x512.png',
+    '/apple-touch-icon.png',
+    '/favicon-16x16.png',
+    '/favicon-32x32.png',
+    '/favicon.ico',
+    '/site.webmanifest'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+}); 
